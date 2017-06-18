358eabc7599ab991e8ff7e5801921362de3021ba
Yang Zhou <404422239@qq.com>
2017-06-18 14:49:20 +0800

    translate and format

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-    pottsSqure a;
-    //Potts a;
-    cout<<"请输入 Lx Ly q 初始条件(0=spin一致,1=spin随机:"<<endl;
-    cin>>a.Lx>>a.Ly>>a.Nq>>a.init;
-cout<<"从第nstart步开始统计 每次执行MCS步 方法(0=metropolis,1=sw,2=wolff):"<<endl;
-cin>>a.nstart>>a.MCS>>a.method;
- cout<<"开始温度 温度间隔 结束温度:"<<endl;
-cin>>a.t_start>>a.t_int>>a.t_end;
-a.run();
+	pottsSqure a;
+	//Potts a;
+	cout<<"PLEASE INPUT : Lx,Ly,q,init_condiction(0: spin parallel,1:spin random)"<<endl;
+	cin>>a.Lx>>a.Ly>>a.Nq>>a.init;
+	cout<<"PLEASE INPUT : nstart,MCS,method(0=metropolis,1=sw,2=wolff):"<<endl;
+	cin>>a.nstart>>a.MCS>>a.method;
+	 cout<<"PLEASE INPUT : t_start,t_interval,t_end"<<endl;
+	cin>>a.t_start>>a.t_int>>a.t_end;
+	a.run();
     return 0;
 }
