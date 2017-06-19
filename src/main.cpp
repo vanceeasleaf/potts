e2de88347c373910fe8cf040a430105c03e0fec8
Yang Zhou <404422239@qq.com>
2017-06-19 22:18:12 +0800

    reformat

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,26 @@
+/*
+* @Author: YangZhou
+* @Date:   2017-06-13 20:13:20
+* @Last Modified by:   YangZhou
+* @Last Modified time: 2017-06-19 22:17:07
+*/
 #include <iostream>
 #include "potts.h"
 #include "pottsSqure.h"
 using namespace std;
 
-int main()
-{
-	pottsSqure a;
-	//Potts a;
-	cout<<"PLEASE INPUT : Lx,Ly,q,init_condiction(0: spin parallel,1:spin random)"<<endl;
-	cin>>a.Lx>>a.Ly>>a.Nq>>a.init;
-	cout<<"PLEASE INPUT : nstart,MCS,method(0=metropolis,1=sw,2=wolff):"<<endl;
-	cin>>a.nstart>>a.MCS>>a.method;
-	 cout<<"PLEASE INPUT : t_start,t_interval,t_end"<<endl;
-	cin>>a.t_start>>a.t_int>>a.t_end;
-	a.run();
-    return 0;
+int main() {
+  pottsSqure a;
+  // Potts a;
+  cout << "PLEASE INPUT : Lx,Ly,q,init_condiction(0: spin parallel,1:spin "
+          "random)"
+       << endl;
+  cin >> a.Lx >> a.Ly >> a.Nq >> a.init;
+  cout << "PLEASE INPUT : nstart,MCS,method(0=metropolis,1=sw,2=wolff):"
+       << endl;
+  cin >> a.nstart >> a.MCS >> a.method;
+  cout << "PLEASE INPUT : t_start,t_interval,t_end" << endl;
+  cin >> a.t_start >> a.t_int >> a.t_end;
+  a.run();
+  return 0;
 }
--- src/pottsSqure.cpp
+++ src/pottsSqure.cpp
@@ -1,80 +1,86 @@
+/*
+* @Author: YangZhou
+* @Date:   2017-06-13 20:13:20
+* @Last Modified by:   YangZhou
+* @Last Modified time: 2017-06-19 22:17:54
+*/
 #include "pottsSqure.h"
 #include <math.h>
-pottsSqure::pottsSqure():Potts()
-{
-    //ctor
+pottsSqure::pottsSqure() : Potts() {
+  // ctor
 }
 
-pottsSqure::~pottsSqure()
-{
-    //dtor
+pottsSqure::~pottsSqure() {
+  // dtor
 }
-void pottsSqure::i2coord(int i,int &xi,int &yi,int &ni){
-    xi=i%Lx;
-    yi=floor((double)i/Lx);
+void pottsSqure::i2coord(int i, int &xi, int &yi, int &ni) {
+  xi = i % Lx;
+  yi = floor((double)i / Lx);
 }
-int pottsSqure::coord2i(int x,int y,int n){
-    int i=y*Lx+x;
-    return i;
+int pottsSqure::coord2i(int x, int y, int n) {
+  int i = y * Lx + x;
+  return i;
 }
-void pottsSqure::nei(int xi,int yi,int ni,int index,int &x,int &y,int &n){
-    switch(index){
+void pottsSqure::nei(int xi, int yi, int ni, int index, int &x, int &y,
+                     int &n) {
+  switch (index) {
     case 0:
-        x=xi+1;y=yi;
-        break;
+      x = xi + 1;
+      y = yi;
+      break;
     case 1:
-        x=xi;y=yi+1;
-        break;
+      x = xi;
+      y = yi + 1;
+      break;
     case 2:
 
-        x=xi-1;y=yi;
-        break;
-            case 3:
+      x = xi - 1;
+      y = yi;
+      break;
+    case 3:
 
-        x=xi;y=yi-1;
-        break;
-    }
-    if(x==Lx)x=0;
-    if(x==-1)x=Lx-1;
-    if(y==Ly)y=0;
-    if(y==-1)y=Ly-1;
+      x = xi;
+      y = yi - 1;
+      break;
+  }
+  if (x == Lx) x = 0;
+  if (x == -1) x = Lx - 1;
+  if (y == Ly) y = 0;
+  if (y == -1) y = Ly - 1;
 }
 
-void pottsSqure::constructNeighbor(){
-    nNei=4;
-    Natom=Lx*Ly;
-            x=new double[Natom];
-    y=new double[Natom];
-    neighbor=new int*[Natom];
-    for(int i=0;i<Natom;i++){
-        neighbor[i]=new int[nNei];
-        x[i]=y[i]=0;
-    }
-        //fstream fneibor("neibor.txt",ios::out);
-    int xx,yy,n;
-    for(int i=0;i<Natom;i++){
-            int xi,yi,ni;
-        i2coord(i,xi,yi,ni);
-        x[i]=xi;
-        y[i]=yi;
+void pottsSqure::constructNeighbor() {
+  nNei = 4;
+  Natom = Lx * Ly;
+  x = new double[Natom];
+  y = new double[Natom];
+  neighbor = new int *[Natom];
+  for (int i = 0; i < Natom; i++) {
+    neighbor[i] = new int[nNei];
+    x[i] = y[i] = 0;
+  }
+  // fstream fneibor("neibor.txt",ios::out);
+  int xx, yy, n;
+  for (int i = 0; i < Natom; i++) {
+    int xi, yi, ni;
+    i2coord(i, xi, yi, ni);
+    x[i] = xi;
+    y[i] = yi;
 
-       // fneibor<<i;
-        for(int j=0;j<nNei;j++){
-            nei(xi,yi,ni,j,xx,yy,n);
-            neighbor[i][j]=coord2i(xx,yy,n);
-            //fneibor<<'\t'<<neighbor[i][j];
-
-        }
-
+    // fneibor<<i;
+    for (int j = 0; j < nNei; j++) {
+      nei(xi, yi, ni, j, xx, yy, n);
+      neighbor[i][j] = coord2i(xx, yy, n);
+      // fneibor<<'\t'<<neighbor[i][j];
     }
- /*   for(int i=0;i<Natom;i++){
-          fneibor<<x[i]<<'\t'<<y[i];
-          for(int j=0;j<3;j++){
-              fneibor<<'\t'<<x[neighbor[i][j]]<<'\t'<<y[neighbor[i][j]];
-          }
-          fneibor<<endl;
-    }
-fneibor.close();
-*/
+  }
+  /*   for(int i=0;i<Natom;i++){
+           fneibor<<x[i]<<'\t'<<y[i];
+           for(int j=0;j<3;j++){
+               fneibor<<'\t'<<x[neighbor[i][j]]<<'\t'<<y[neighbor[i][j]];
+           }
+           fneibor<<endl;
+     }
+ fneibor.close();
+ */
 }
-
